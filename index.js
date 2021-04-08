8b7d52b4a326e28243725dfb727c5e4badd90396
unknown <you@example.com>
2021-04-08 21:24:54 +0300

    cw16

---
+++ index.js
@@ -0,0 +1,32 @@
+// // const str = palindomChck('Anna');
+
+// // function palindomChck (string) {
+// //     string = string.split('');
+// //     let rev = [];
+
+// //     for(i = string.length - 1; i >= 0; i--){
+// //         rev.push(string[i])
+// //     }
+   
+// //     rev = rev.join('');
+// //     let x = rev.toUpperCase();
+// //     string = string.join('');
+// //     let y = string.toUpperCase();
+// //     return console.log(y == x? true : false);
+   
+// // }
+
+// const vowels = ['a', 'o', 'u', 'e','i'];
+// const string = 'Hello world';
+// str (vowels, string)
+// function str (str_1, str_2) {
+//     str_2 = str_2.split('');
+//     console.log(str_2);
+//     for(let i = 0; i < str_1.length; i++){
+//         for(let j = 0; j < str_2.length; j++){
+//             if (str_1[i] == str_2[j]){
+//                 console.log (str_2[j]);
+//         }
+//     }
+// }     
+// }
